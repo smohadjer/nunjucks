c7f7b53bf3cffc7d5e6827cfc16729113d845c11
Saeid Mohadjer <saeid@fastmail.fm>
2016-05-04 20:50:42 +0200

    added handlebars.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,9 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-	gulp.watch('app/resources/css/*', ['sass']);
-});
-
-gulp.task('sass', function () {
-	var stream = gulp.src('app/resources/css/styles.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('app/resources/css'));
-	return stream;
+	//gulp.watch('app/resources/css/*', ['sass']);
 });
 
 // serve development templates
-gulp.task('serve', ['sass', 'watch']);
+gulp.task('serve', ['watch']);
