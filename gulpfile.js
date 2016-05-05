84be4d745d4557e3e2c76244ae39155ebb14dcd9
Saeid Mohadjer <saeid@fastmail.fm>
2016-05-05 18:36:44 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,30 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	opn = require('opn'),
+	connect = require('gulp-connect');
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-	//gulp.watch('app/resources/css/*', ['sass']);
+	gulp.watch('app/resources/css/*', ['sass']);
+});
+
+gulp.task('sass', function () {
+	var stream = gulp.src('app/resources/css/styles.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('app/resources/css'));
+	return stream;
+});
+
+gulp.task('connect', function() {
+  connect.server({
+	  port: 8080,
+	  root: 'app'
+  });
+});
+
+gulp.task('open', ['connect'], function () {
+    return opn( 'http://localhost:8080' );
 });
 
 // serve development templates
-gulp.task('serve', ['watch']);
+gulp.task('serve', ['open', 'sass', 'watch']);
