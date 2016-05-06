9047befa47a9cf0e22ee8a54e93853461ec023b9
Saeid Mohadjer <saeid@fastmail.fm>
2016-05-06 10:59:02 +0200

    added handlebars precompile example

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ var gulp = require('gulp'),
 	opn = require('opn'),
 	connect = require('gulp-connect');
 
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var concat = require('gulp-concat');
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
 	gulp.watch('app/resources/css/*', ['sass']);
@@ -13,6 +18,20 @@ gulp.task('sass', function () {
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('app/resources/css'));
 	return stream;
+});
+
+gulp.task('templates', function(){
+  gulp.src('app/resources/templates/*.hbs')
+    .pipe(handlebars({
+      handlebars: require('handlebars')
+	}))
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'MyApp.templates',
+      noRedeclare: true, // Avoid duplicate declarations
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('app/resources/templates/'));
 });
 
 gulp.task('connect', function() {
@@ -27,4 +46,4 @@ gulp.task('open', ['connect'], function () {
 });
 
 // serve development templates
-gulp.task('serve', ['open', 'sass', 'watch']);
+gulp.task('serve', ['open', 'templates', 'sass', 'watch']);
